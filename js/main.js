0b65ff7677e8eb8353eea6cbaef2155f2e4a717f
Gustavo Salinas <imanolsalinas1996@gmail.com>
2020-07-02 19:50:41 -0300

    Menu anclas

--- js/main.js
+++ js/main.js
@@ -63,6 +63,7 @@
   $('.nonloop-block-11').owlCarousel({
       center: false,
       items: 1,
+      autoplay: true,
       loop: false,
       stagePadding: 20,
       margin:50,
@@ -186,6 +187,40 @@
     location.href = this.href;
   });
 
-
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
 })(jQuery);
