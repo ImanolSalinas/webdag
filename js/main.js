26664128fc60104661266b2b5b515c761ba3011d
Lucas Jose Serena <serenalucasjose@gmail.com>
2020-07-02 20:00:42 -0300

    Back to top button

--- js/main.js
+++ js/main.js
@@ -1,95 +1,95 @@
-(function($) {
+(function ($) {
 
-	'use strict';
+  'use strict';
 
-	// bootstrap dropdown hover
+  // bootstrap dropdown hover
 
   // loader
-  var loader = function() {
-    setTimeout(function() { 
-      if($('#loader').length > 0) {
+  var loader = function () {
+    setTimeout(function () {
+      if ($('#loader').length > 0) {
         $('#loader').removeClass('show');
       }
     }, 1);
   };
   loader();
 
-	
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			$this.find('.dropdown-menu').removeClass('show');
-	});
+
+  $('nav .dropdown').hover(function () {
+    var $this = $(this);
+    $this.addClass('show');
+    $this.find('> a').attr('aria-expanded', true);
+    $this.find('.dropdown-menu').addClass('show');
+  }, function () {
+    var $this = $(this);
+    $this.removeClass('show');
+    $this.find('> a').attr('aria-expanded', false);
+    $this.find('.dropdown-menu').removeClass('show');
+  });
 
 
-	$('#dropdown04').on('show.bs.dropdown', function () {
-	  console.log('show');
-	});
+  $('#dropdown04').on('show.bs.dropdown', function () {
+    console.log('show');
+  });
 
-	// home slider
-	$('.home-slider').owlCarousel({
-    loop:true,
+  // home slider
+  $('.home-slider').owlCarousel({
+    loop: true,
     autoplay: true,
-    margin:0,
+    margin: 0,
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',
-    nav:true,
+    nav: true,
     autoplayHoverPause: true,
     items: 1,
     dragTouch: true,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
       },
-      600:{
-        items:1,
-        nav:false
+      600: {
+        items: 1,
+        nav: false
       },
-      1000:{
-        items:1,
-        nav:true
+      1000: {
+        items: 1,
+        nav: true
       }
     }
-	});
+  });
 
   $('.nonloop-block-11').owlCarousel({
-      center: false,
-      items: 1,
-      autoplay: true,
-      loop: false,
-      stagePadding: 20,
-      margin:50,
-      nav: true,
-      smartSpeed: 1000,
-      navText: ['<span class="ion-chevron-left">', '<span class="ion-chevron-right">'],
-      responsive:{
-        600:{
-          stagePadding: 20,
-          items:1
-        },
-        800:{
-          stagePadding: 20,
-          items:1
-        },
-        1000:{
-          // stagePadding: 200,
-          items:1
-        }
+    center: false,
+    items: 1,
+    autoplay: true,
+    loop: false,
+    stagePadding: 20,
+    margin: 50,
+    nav: true,
+    smartSpeed: 1000,
+    navText: ['<span class="ion-chevron-left">', '<span class="ion-chevron-right">'],
+    responsive: {
+      600: {
+        stagePadding: 20,
+        items: 1
+      },
+      800: {
+        stagePadding: 20,
+        items: 1
+      },
+      1000: {
+        // stagePadding: 200,
+        items: 1
       }
-    });
+    }
+  });
 
-	// owl carousel
-	var majorCarousel = $('.js-carousel-1');
-	majorCarousel.owlCarousel({
-    loop:true,
+  // owl carousel
+  var majorCarousel = $('.js-carousel-1');
+  majorCarousel.owlCarousel({
+    loop: true,
     autoplay: true,
     stagePadding: 7,
     margin: 20,
@@ -98,28 +98,28 @@
     nav: true,
     autoplayHoverPause: true,
     items: 3,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
       },
-      600:{
-        items:2,
-        nav:false
+      600: {
+        items: 2,
+        nav: false
       },
-      1000:{
-        items:3,
-        nav:true,
-        loop:false
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false
       }
-  	}
-	});
+    }
+  });
 
-	// owl carousel
-	var major2Carousel = $('.js-carousel-2');
-	major2Carousel.owlCarousel({
-    loop:true,
+  // owl carousel
+  var major2Carousel = $('.js-carousel-2');
+  major2Carousel.owlCarousel({
+    loop: true,
     autoplay: true,
     stagePadding: 7,
     margin: 20,
@@ -128,99 +128,118 @@
     nav: true,
     autoplayHoverPause: true,
     items: 4,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
       },
-      600:{
-        items:3,
-        nav:false
+      600: {
+        items: 3,
+        nav: false
       },
-      1000:{
-        items:4,
-        nav:true,
-        loop:false
+      1000: {
+        items: 4,
+        nav: true,
+        loop: false
       }
-  	}
-	});
+    }
+  });
 
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.element-animate').waypoint( function( direction ) {
+  var contentWayPoint = function () {
+    var i = 0;
+    $('.element-animate').waypoint(function (direction) {
 
-			if( direction === 'down' && !$(this.element).hasClass('element-animated') ) {
-				
-				i++;
+      if (direction === 'down' && !$(this.element).hasClass('element-animated')) {
 
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+        i++;
 
-					$('body .element-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn element-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft element-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight element-animated');
-							} else {
-								el.addClass('fadeInUp element-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 100);
-					});
-					
-				}, 100);
-				
-			}
+        $(this.element).addClass('item-animate');
+        setTimeout(function () {
 
-		} , { offset: '95%' } );
-	};
-	contentWayPoint();
+          $('body .element-animate.item-animate').each(function (k) {
+            var el = $(this);
+            setTimeout(function () {
+              var effect = el.data('animate-effect');
+              if (effect === 'fadeIn') {
+                el.addClass('fadeIn element-animated');
+              } else if (effect === 'fadeInLeft') {
+                el.addClass('fadeInLeft element-animated');
+              } else if (effect === 'fadeInRight') {
+                el.addClass('fadeInRight element-animated');
+              } else {
+                el.addClass('fadeInUp element-animated');
+              }
+              el.removeClass('item-animate');
+            }, k * 100);
+          });
 
-   $('.navbar .dropdown > a').click(function(){
+        }, 100);
+
+      }
+
+    }, {
+      offset: '95%'
+    });
+  };
+  contentWayPoint();
+
+  $('.navbar .dropdown > a').click(function () {
     location.href = this.href;
   });
 
   $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
       }
+    });
+
+  var btn = $('#button');
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
     }
   });
+
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, '300');
+  });
+
 
 })(jQuery);
